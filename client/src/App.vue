ed4557897793bada46a8d3450f09f2253449405d
Adedero <118962395+Adedero@users.noreply.github.com>
2024-10-21 00:00:32 +0100

    ugghu

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <Toast position="bottom" class="w-dvw md:hidden fixed  left-1/2 -translate-x-1/2" />
-  <Toast class="max-w-96 hidden md:block" />
+  <Toast class="max-w-96" />
   <ConfirmDialog class="max-w-96" />
   <RouterView />
 </template>
