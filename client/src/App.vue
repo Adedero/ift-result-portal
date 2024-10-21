189166599c76ce3418f52b2529b500026c13e459
Adedero <118962395+Adedero@users.noreply.github.com>
2024-10-21 01:03:48 +0100

    Fixed toast position

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Toast class="max-w-96" />
+  <Toast class="w-[95dvw] left-1/2 -translate-x-1/2 md:left-[unset] md:-translate-x-0 md:max-w-96" />
   <ConfirmDialog class="max-w-96" />
   <RouterView />
 </template>
